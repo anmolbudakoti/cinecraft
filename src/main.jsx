708a8063d01039eeeb2e317141e260e311ad7128
anmolbudakoti <anmolbudakoti123@gmail.com>
2024-12-01 15:25:27 +0530

    fixed router

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
     <MovieContextState>
-    <BrowserRouter>
+    <BrowserRouter basename='/cinecrate'>
     <App />
     </BrowserRouter>
     </MovieContextState>
